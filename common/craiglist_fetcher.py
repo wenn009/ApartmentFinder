b670dbc707fc51321a33c0a379769019147a44c2
wenn009 <wenn009@gmail.com>
2017-09-15 00:44:12 -0400

    add zip-code

--- common/craiglist_fetcher.py
+++ common/craiglist_fetcher.py
@@ -7,8 +7,8 @@ from craigslist import CraigslistHousing
 for result in cl_h.get_results(sort_by='newest', geotagged=True):
     print result'''
 #CraigslistHousing.show_filters()
-def fetch_housing(site, area, max_price, zip_code):
+def fetch_housing(site, area, zip_code):
     houses = CraigslistHousing(site=site, area=area, category='aap', 
-                            filters={'max_price': max_price, 'private_room':True,
+                            filters={ 'private_room':True,
                                         'zip_code': zip_code })
     return houses
--- common/craiglist_fetcher_test.py
+++ common/craiglist_fetcher_test.py
@@ -1,7 +1,7 @@
 import housing_fetcher
 
 def test_basic():
-    houses = housing_fetcher.fetch_housing('newyork','brk','1500', '11214')
+    houses = housing_fetcher.fetch_housing('newyork','brk', '11214')
     #assert type(houses) is object
     print 'test_basic passed.'
     for result in houses.get_results(sort_by='newest', geotagged=True, limit=5):
